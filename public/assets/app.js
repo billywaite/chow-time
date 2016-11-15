57b4987736cc0e4f05c1d9fdf894bfb46b6fe134
Billy Waite <billy.waite91@gmail.com>
2016-11-15 09:17:34 -0500

    Using ajax call for the api instead of node

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,1 +1,18 @@
-//put jquery in this file to insert the JSON from the api into the HTML
+$(document).ready(function() {
+
+// Create a form to input text
+var keywords = $('#input-text').val();
+
+// use the keywords variable inplace of chicken
+var url = "http://api.yummly.com/v1/api/recipes?q=chicken&_app_id=5c392f3d&_app_key=3d62beeb20098f02fb88f93933ae92f1";
+
+$.ajax({
+  type: 'GET',
+  url: url,
+  dataType: 'jsonp',
+  success: function() { console.log('succes!'); },
+  error: function(data, data2) { console.log(data); },
+  });
+});
+
+// display the json data
